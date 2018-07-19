b4eebf751e7386e92f419e7e98245ef60cb43101
SergioPerez <dark07perez@gmail.com>
2018-07-19 03:12:32 -0500

    poblar estudiantes
    terminado

--- WindowsFormsApp1/WindowsFormsApp1/Clases/Poblar.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Clases/Poblar.cs
@@ -22,25 +22,25 @@ namespace WindowsFormsApp1.Clases
         private void PoblarEstudiantes()
         {
             Estudiante est1 = new Estudiante("Manuel Sergio","Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] {"Lunes: 08:00","Martes: 15:00","Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est2 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est3 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est4 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est5 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est6 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est7 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est8 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est9 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est10 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est11 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est12 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est13 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est14 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est15 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est16 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est17 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est18 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est19 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
-            Estudiante est20 = new Estudiante("Manuel Sergio", "Perez Espitia", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est2 = new Estudiante("Juan Francisco", "Gonzalez Rojas", 2084034, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est3 = new Estudiante("Dulce Maríao", "Rodríguez Muñoz", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est4 = new Estudiante("Sara Sofía", "Gómez Ramírez", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est5 = new Estudiante("Luisa Fernanda", "López Jiménez", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est6 = new Estudiante("Sara Valentina", "González Torres", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est7 = new Estudiante("María Fernanda", "García González", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est8 = new Estudiante("Ana Sofía", "Martínez Sánchez", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est9 = new Estudiante("Ana Lucía", "Ramírez Vargas", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est10 = new Estudiante("María Camila", "Sánchez Moreno", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est11 = new Estudiante("Juan José", "Hernández Martínez", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est12 = new Estudiante("Miguel Angel", "Díaz Rojas", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est13 = new Estudiante("Juan Sebastián", "Perez Martínez", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est14 = new Estudiante("Juan David", "Torres Muñoz", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est15 = new Estudiante("Samuel David", "Rojas Hernández", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est16 = new Estudiante("Juan Pablo", "Vargas Díaz", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est17 = new Estudiante("Andrés Felipe", "Moreno Moreno", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est18 = new Estudiante("Juan Esteban", "Gutiérrez Sánchez", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est19 = new Estudiante("Juan Diego", "Jiménez  Ramírez", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
+            Estudiante est20 = new Estudiante("Angel David", "Muñoz Martínez", 2095112, "Ing de Sistemas", 10, true, new string[] { "Lunes: 08:00", "Martes: 15:00", "Miercoles: 13:00" }, 0, "Ninguna");
 
         }
     }    
